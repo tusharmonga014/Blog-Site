b560702ac913fc792b8e84158b9007788f844589
Tushar <tusharmongatt@gmail.com>
2021-03-20 01:40:14 +0530

    Add post word limit feature

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+document.getElementById("inputText").addEventListener("keyup", function () {
+    let data = document.getElementById("inputText").value;
+    document.getElementsByClassName("limit")[0].innerHTML = " &nbsp;&nbsp;(" + (700 - data.length) + " characters)";
+    if (data.length > 700) {
+        document.getElementsByClassName("limit")[0].classList.add("limit-reached");
+        document.getElementById("publish").disabled = true;
+    }
+    else {
+        document.getElementById("publish").disabled = false;
+        document.getElementsByClassName("limit")[0].classList.remove("limit-reached");
+    }
+})
