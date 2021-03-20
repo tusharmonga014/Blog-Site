09ede477823a2445dca03e3af3ee36807082d62e
Tushar <tusharmongatt@gmail.com>
2021-03-20 18:07:23 +0530

    fixed port

--- app.js
+++ app.js
@@ -84,6 +84,12 @@ app.get("*", function (req, res) {
   res.render("error");
 })
 
-app.listen(3000, function () {
+let port=process.env.PORT;
+if(port==null || port=="")
+{
+  port=3000;
+}
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
